a11db82596e8b6ab7c433c92f1e7f6b6dc463a95
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2024-08-21 22:38:34 +0900

    feat: create Top page

---
+++ lib/pages/top/index.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class Top extends StatelessWidget {
+  const Top({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = useState(0);
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Top'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              counter.toString(),
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counter.value += 1;
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
