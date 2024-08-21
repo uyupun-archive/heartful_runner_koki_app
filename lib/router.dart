6b0758bfbd382060c4047f7c9d43b20743dffb0c
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2024-08-21 22:38:57 +0900

    feat: create router

---
+++ lib/router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:spajam_24_app/pages/top/index.dart';
+
+part 'router.g.dart';
+
+@TypedGoRoute<TopRoute>(
+  path: '/',
+)
+class TopRoute extends GoRouteData {
+  const TopRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) => const Top();
+}
+
+final router = GoRouter(
+  debugLogDiagnostics: true,
+  initialLocation: '/',
+  routes: $appRoutes,
+);
