8ccb380df0f31ea4d365d330aecd22b264dfdb79
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2024-08-25 03:11:19 +0900

    feat: add SharedPreferences settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:spajam_24_app/common/provider/shared_preferences.dart';
 import 'package:spajam_24_app/router.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  late final SharedPreferences sharedPreferences;
+  await Future.wait([
+    Future(() async {
+      sharedPreferences = await SharedPreferences.getInstance();
+    }),
+  ]);
+
+  final overrides = [
+    sharedPreferencesProvider.overrideWithValue(sharedPreferences),
+  ];
+
   runApp(
-    const ProviderScope(
-      child: MyApp(),
+    ProviderScope(
+      overrides: overrides,
+      child: const MyApp(),
     ),
   );
 }
