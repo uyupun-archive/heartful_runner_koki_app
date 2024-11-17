3f1445d5e9f6dad3a46ddfd4fc6948e66b3eda56
Hideaki Takahashi <hi_takahashi@yumemi.co.jp>
2024-11-17 17:14:57 +0900

    feat: add SingleChildScrollView

--- lib/pages/relay/message.dart
+++ lib/pages/relay/message.dart
@@ -18,72 +18,74 @@ class RelayMessagePage extends HookConsumerWidget {
       appBar: AppBar(
         title: const Text('寄せ書きを届ける'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text('自分の名前'),
-            const SizedBox(height: 8),
-            TextField(
-              controller: writerNameController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 24),
-            const Text('メッセージ'),
-            const SizedBox(height: 8),
-            TextField(
-              controller: contentController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-              keyboardType: TextInputType.multiline,
-              maxLines: 3,
-            ),
-            const SizedBox(height: 40),
-            Visibility(
-              visible: isError.value,
-              child: const Column(
-                children: [
-                  Text(
-                    '寄せ書きの作成に失敗しました。',
-                    style: TextStyle(color: Colors.red),
-                  ),
-                  SizedBox(height: 40),
-                ],
-              ),
-            ),
-            Center(
-              child: TextButton(
-                onPressed: () async {
-                  isError.value = false;
-
-                  final isSuccess = await ref.read(relayMessageProvider(
-                    writerName: writerNameController.text,
-                    content: contentController.text,
-                  ).future);
-
-                  if (isSuccess) {
-                    const RelayedPageRoute().go(context);
-                  } else {
-                    isError.value = true;
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                child: const Text(
-                  '届ける',
-                  style: TextStyle(color: Colors.black),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('自分の名前'),
+              const SizedBox(height: 8),
+              TextField(
+                controller: writerNameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
                 ),
               ),
-            )
-          ],
+              const SizedBox(height: 24),
+              const Text('メッセージ'),
+              const SizedBox(height: 8),
+              TextField(
+                controller: contentController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+              ),
+              const SizedBox(height: 40),
+              Visibility(
+                visible: isError.value,
+                child: const Column(
+                  children: [
+                    Text(
+                      '寄せ書きの作成に失敗しました。',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                    SizedBox(height: 40),
+                  ],
+                ),
+              ),
+              Center(
+                child: TextButton(
+                  onPressed: () async {
+                    isError.value = false;
+
+                    final isSuccess = await ref.read(relayMessageProvider(
+                      writerName: writerNameController.text,
+                      content: contentController.text,
+                    ).future);
+
+                    if (isSuccess) {
+                      const RelayedPageRoute().go(context);
+                    } else {
+                      isError.value = true;
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: const Text(
+                    '届ける',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
